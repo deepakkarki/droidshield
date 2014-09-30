aaadcf026f9f785e8dd3c20cdb986a89cd92ccf9
deepakkarki <deepak6k@gmail.com>
2014-09-30 21:55:58 +0530

    added more code

--- android/src/com/example/droidshield/DroidConnect.java
+++ android/src/com/example/droidshield/DroidConnect.java
@@ -9,50 +9,74 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 import android.app.Activity;
 /*
- * This class acts as a wrapper for accessing bluetooth 
+ * This class acts as a wrapper for accessing bluetooth device
  * Makes life simpler to have this here 
  */
 
 public class DroidConnect {
 	
 	//adapter object representing the bluetooth module
-	BluetoothAdapter btAdpt;
+	private BluetoothAdapter btAdpt;
 	
 	//socket opened via the adapter to stream the data
 	//we get the input and output stream object from sock
-    BluetoothSocket sock;
+    private BluetoothSocket sock;
     
     //the reference to the device we connect to
-    BluetoothDevice dev;
+    private BluetoothDevice dev;
     
     //corresponding in/out put stream 
-    InputStream dev_in;
-    OutputStream dev_out;
+    private InputStream dev_in;
+    private OutputStream dev_out;
     
     //48 bit MAC identifier for the bluetooth module. 
-    String dev_mac;
+    private String dev_mac;
     
     //whether or not the connection is established 
-    boolean connected;
+    private boolean connected;
+    
+    public DroidConnect()
+	{
+		btAdpt = BluetoothAdapter.getDefaultAdapter();
+		connected = false;
+	}
+	
     
 	public DroidConnect(String mac)
 	{
 		btAdpt = BluetoothAdapter.getDefaultAdapter();
-		
+		connected = false;
 		dev_mac = mac;
+	}
+	
+	public void setMAC(String mac)
+	{
+		dev_mac = mac;
+	}
+	
+	public boolean setDevice()
+	{
+		//This function opens a chooser for the user to choose bt device
+		//if everything okay, call setMAC, and then return true
+		//else return false
+		return false;
 	}
 	
 	public boolean connect()
 	{
-		
+		if(dev_mac == null){
+			//TODO make toast and exit
+			return false;
+		}
 		//make sure bt module is enabled.
-		while(!btAdpt.isEnabled())
-		{
+		while(!btAdpt.isEnabled()){
 		   Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 		   //TODO : check if this would even work!
 		   new Activity().startActivityForResult(enableBluetooth, 0);
@@ -60,8 +84,7 @@ public class DroidConnect {
 		
 		UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
 		
-		try
-		{
+		try{
 			//get the device with the required mac address
 			dev = btAdpt.getRemoteDevice(dev_mac);
 			
@@ -77,9 +100,8 @@ public class DroidConnect {
 	        connected = true;
 	        return true;
 		}
-		catch(Exception e)
-		{
-			//make a toast?
+		catch(IOException e){
+			//make a toast? --no, let caller handle it!
 			return false; 
 		}
 	}
@@ -89,16 +111,44 @@ public class DroidConnect {
 		if(connected){
 			int c = (int)ch;
 			
-			try
-			{	
+			try{	
 				dev_out.write(c);
 			}
 			
-			catch(Exception e)
-			{
+			catch(IOException e){
+				//TODO : do what if the device itself is not connected?
+			}
+		}
+	}
+	
+	public void send(byte[] data)
+	{
+		if(connected)
+		{
+			try{
+				dev_out.write(data);
+			}
+			
+			catch(IOException e){
 				
 			}
 		}
+	}
+	
+	public byte[] read(int num)
+	{
+		byte [] data =  new byte[num];
+		if (connected){
+			
+			try{
+				dev_in.read(data);
+			}
+			
+			catch(IOException e){
+				
+			}
+		}
+		return data;
 	}
 	
 	public byte readByte()
@@ -106,16 +156,15 @@ public class DroidConnect {
 		//does it make sense to r/w in terms of int instead?? 
 		if(connected){
 			
-			try
-			{	
+			try{	
 				return (byte) dev_in.read();
 			}
 			
-			catch(Exception e)
-			{
+			catch(IOException e){
 				return (byte)0;
 			}
 		}
+		//TODO : what do I return if there was a problem
 		return (Byte) null;
 	}
 	
@@ -128,10 +177,15 @@ public class DroidConnect {
 		        dev_out.close();
 		        dev_in.close();
 		        sock.close();
+		        connected = false;
 			}
 			
 			catch (Exception e){
 			}
 		}
     }
+
+	public boolean isConnected() {
+		return this.connected;
+	}
 }
--- android/src/com/example/droidshield/MainActivity.java
+++ android/src/com/example/droidshield/MainActivity.java
@@ -5,9 +5,15 @@
 package com.example.droidshield;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.droidshield.DroidConnect;
 /*
@@ -18,10 +24,64 @@ import com.example.droidshield.DroidConnect;
 
 public class MainActivity extends ActionBarActivity {
 
+	DroidConnect dev;
+	OnClickListener connect = new OnClickListener() 
+	{
+		@Override
+		public void onClick(View conn_button) {
+			Button con = (Button)conn_button;
+			
+			//device is not connected
+			if(!dev.isConnected()){
+				//TODO : replace the following hard code with code to scan bt device
+				
+				//add code to select mac of bluetooth
+				dev.setMAC("00:12:12:24:16:30"); 
+				
+				//try to connect
+				if(dev.connect()){
+					con.setText("Disconnect");
+					TextView status = (TextView) findViewById(R.id.status_text);
+					status.setText("Connected to device!");
+				}
+				
+				else{
+					//make a TOAST and warn that connection FAILED
+					Context context = getApplicationContext();
+
+					Toast toast = Toast.makeText(context, "Failed to connect!", Toast.LENGTH_SHORT);
+					toast.show();
+				}
+			}
+			
+			//else device is connected
+			
+			else{
+				dev.close();
+				con.setText("Connect to H/W");
+				TextView status = (TextView) findViewById(R.id.status_text);
+				status.setText("");
+			}
+		}
+	};
+	
+	OnClickListener start_service = new OnClickListener() 
+	{
+		@Override
+		public void onClick(View conn_button) {
+			//start the service -- which is not yet written
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		dev = new DroidConnect();
+		Button conn = (Button)findViewById(R.id.connect_button);
+		Button service = (Button)findViewById(R.id.start_service);
+		conn.setOnClickListener(connect);
+		service.setOnClickListener(start_service);
 	}
 
 	@Override
