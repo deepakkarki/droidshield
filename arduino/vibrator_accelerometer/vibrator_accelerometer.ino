2f287b650abadbd40aa547e82e14da47e8b9d29d
deepakkarki <deepak6k@gmail.com>
2014-10-15 23:41:52 +0530

    vibratoe+accelerometer

---
+++ arduino/vibrator_accelerometer/vibrator_accelerometer.ino
@@ -0,0 +1,59 @@
+int pin = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin, OUTPUT);
+  init_shield(); 
+}
+
+void loop() {
+  
+  //if current acc data is > 5
+  if(getAccData() > 5){
+    //vibrate the phone
+    vibrate();
+  }
+  
+  else{
+    //set output pin to high
+    digitalWrite(pin, HIGH);
+  }
+  
+  //wait
+  delay(1200);
+  
+  //set output pin to low again===
+  digitalWrite(pin, LOW);  
+}
+
+
+
+/****************** API *****************/
+
+
+void init_shield() {
+  while(!Serial.available()){}
+  Serial.read();//init byte
+  digitalWrite(pin, HIGH);
+  delay(1000);
+  digitalWrite(pin, LOW);
+}
+
+int getAccData()
+{
+  //request for accelerometer data
+  Serial.write((char)1); 
+  Serial.write((char)1);
+  while(Serial.available() == 0){
+    //wait
+  }
+  
+  return Serial.read();
+}
+
+void vibrate(int time)
+{
+   //vibrate for one second
+    Serial.write(129);
+    Serial.write(1);
+}
